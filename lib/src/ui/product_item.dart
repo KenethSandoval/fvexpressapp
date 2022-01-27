653af2d264c23868a4fc1895b1ec788e036c049d
KenethSandoval <kenethra.74@gmail.com>
2022-01-26 19:14:31 -0600

    fixed: add padding in the card

--- lib/src/ui/product_item.dart
+++ lib/src/ui/product_item.dart
@@ -10,6 +10,34 @@ class ProductsItem extends StatelessWidget {
 	Widget build(BuildContext context) {
 		return Container(
 			padding: const EdgeInsets.all(15),
+			child: Card(
+				child: InkWell(
+					splashColor: Colors.blue.withAlpha(30),
+					onTap: () {
+						debugPrint('Card tapped');
+					},
+					child: SizedBox(
+						width: 500,
+						height: 300,
+						child: Text(title),
+					),
+				), //InkWell
+			), //Card
+		); //Container
+	}
+}
+
+/*
+class ProductsItem extends StatelessWidget {
+	final String title;
+	final Color color;
+
+	ProductsItem(this.title, this.color);
+
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			padding: const EdgeInsets.all(15),
 			child: Text(title),
 			decoration: BoxDecoration(
 				gradient: LinearGradient(
@@ -24,4 +52,4 @@ class ProductsItem extends StatelessWidget {
 			), //BoxDecoration
 		); //Container
 	}
-}
+}*/
